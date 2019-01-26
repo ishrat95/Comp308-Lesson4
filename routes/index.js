8b0cfe6fb103738ab03648bb6a6e5fddd2bf9b16
Your Name <you@example.com>
2019-01-26 12:15:51 -0500

    Icon Higlighting

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,27 @@ let router = express.Router();
 
 /* GET home page. */
 router.get('/',(req, res, next) => {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
 });
+
+router.get('/about',(req, res, next) => {
+  res.render('index', { title: 'About' });
+});
+
+
+router.get('/contact',(req, res, next) => {
+  res.render('index', { title: 'Contact' });
+});
+
+
+router.get('/product',(req, res, next) => {
+  res.render('index', { title: 'Products' });
+});
+
+
+router.get('/service',(req, res, next) => {
+  res.render('index', { title: 'Services' });
+});
+
 
 module.exports = router;
